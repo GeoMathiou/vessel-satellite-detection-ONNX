5a1cee7131db112939d53b6dd6f3fb6aa850c5ab
George <mathmilos3@gmail.com>
2025-06-30 18:33:43 +0300

    minor change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { SatelliteDetectionComponent } from './satellite-detection/satellite-detection.component';
@@ -13,6 +15,7 @@ import { SatelliteDetectionComponent } from './satellite-detection/satellite-det
   declarations: [AppComponent, SatelliteDetectionComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
